193b05d0951ef9db910d931bb6b2e63fb770397f
sksoumik <sadmanks@gmail.com>
2022-07-30 12:47:02 +0600

    add tflite model

--- ml/train.py
+++ ml/train.py
@@ -1,5 +1,41 @@
+from numpy import save
 from data_generator import create_data_generator, count_images
 from model import build_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+import tensorflow as tf
+
+
+def train_model(model, train_generator, validation_generator):
+    model.compile(
+        optimizer=Adam(lr=0.0001),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    model.fit(
+        train_generator,
+        epochs=10,
+        validation_data=validation_generator,
+        callbacks=[
+            ModelCheckpoint(
+                "../model/",
+                monitor="val_loss",
+                verbose=1,
+                save_best_only=True,
+                mode="min",
+            ),
+            EarlyStopping(
+                monitor="val_loss",
+                patience=10,
+                verbose=1,
+                mode="min",
+            ),
+        ],
+    )
+
+    return model
+
 
 if __name__ == "__main__":
     TRAIN_DIR = "../dataset/seg_train/seg_train/"
@@ -25,3 +61,8 @@ if __name__ == "__main__":
     input_shape = (150, 150, 3)
     num_classes = len(class_mapping)
     model = build_model(input_shape, num_classes)
+    model = train_model(model, train_generator, validation_generator)
+
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = converter.convert()
+    open("../model/model.tflite", "wb").write(tflite_model)
